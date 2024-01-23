5c1662153bb9fdf17114a24334c9d2936b281b8c
Kahon7586 <kahon7586@gmail.com>
2024-01-23 10:31:02 +0800

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 import wordList from './Data/wordList.json'
-import { getRandomNum } from './Utils/getRandomNum'
+import HangmanDrawing from './Components/HangmanDrawing'
+import HangmanWord from './Components/HangmanWord'
+import HangmanKeyboard from './Components/HangmanWord'
 
 function App() {
 
-  const [wordToGuess, setWordToGuess] = useState( wordList[getRandomNum(0, wordList.length)] )
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    const randomWord = wordList[ Math.floor(Math.random() * wordList.length) ]
+    return randomWord
+  })
+
+  const [guessedLetter, setGuessedLetter] = useState<string[]>([])
 
   return (
-    <>
-      test
-    </>
+    <div className='interface'>
+      <div className='result'>Lose Win</div>
+      <HangmanDrawing/>
+      <HangmanWord/>
+      <HangmanKeyboard/>
+    </div>
   )
 }
 
