669b5c468d753bb3243392080b03177968056beb
Kahon7586 <kahon7586@gmail.com>
2024-01-24 13:04:40 +0800

    show missing letter when gameover

--- src/Components/HangmanWord.tsx
+++ src/Components/HangmanWord.tsx
@@ -1,29 +1,49 @@
 import { useEffect } from 'react'
 import { useWord } from '../Context/useWord'
 import './HangmanWord.css'
+import { useUpdateEffect } from '../Utils/useUpdateEffect'
 
 
 
 const HangmanWord = (  ) => {
 
-  const { wordToGuess, guessedLetter} = useWord()
+  const { wordToGuess, guessedLetter, gameStatus, setGuessWrongTime} = useWord()
 
-  useEffect( () => {
+  useUpdateEffect( () => {
 
     const [finalLetter] = guessedLetter.slice(-1)
+    if(finalLetter === undefined) return 
+    
     const isLetterGuessed: boolean = wordToGuess.includes(finalLetter)
 
     if(isLetterGuessed){
       const targetList = document.querySelectorAll(`.letter-${finalLetter}`)
-      targetList.forEach(target => target.classList.remove("hide"))
+      targetList.forEach(target => target.classList.remove("unguessed"))
+      console.log('correct')
+      return
+    }
+
+    if(!isLetterGuessed){
+      setGuessWrongTime( prev => prev + 1 )
+      console.log('wrong')
+      return
     }
 
   }, [guessedLetter] )
 
+  useEffect( () => {
+
+    if( gameStatus === "lose"){
+      const targetList = document.querySelectorAll(`.unguessed`)
+      targetList.forEach( target => target.classList.add("failed") )
+    }
+
+  }, [gameStatus])
+
   return (
     <div className='word-container'>
       {wordToGuess.map( (letter, index) => {
-        return <div key={`${letter}-${index}`} className={`letter-container letter-${letter} hide`}>{letter}</div>
+        return <div key={`${letter}-${index}`} className={`letter-container letter-${letter} unguessed`}>{letter}</div>
       } )}
     </div>
   )
