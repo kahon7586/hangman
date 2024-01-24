dcc9195200e6d389906dc435afe2ee7310df8aaa
Kahon7586 <kahon7586@gmail.com>
2024-01-24 13:05:49 +0800

    add more state to track

--- src/Context/WordContextProvider.tsx
+++ src/Context/WordContextProvider.tsx
@@ -1,14 +1,16 @@
-import { ReactNode, useEffect, useState } from "react"
+import { ReactNode, useState } from "react"
 import wordList from '../Data/wordList.json'
 import { WordContext, wordContext } from "./useWord"
+import { useUpdateEffect } from "../Utils/useUpdateEffect"
+import { GameStatus } from "./useWord"
 
 interface WordContextProviderProps {
   children: ReactNode
 }
 
-
 const WordContextProvider = ( {children}: WordContextProviderProps ) => {
 
+  const [gameStatus, setGameStatus] = useState<GameStatus>("playing")
   
   const [wordToGuess, setWordToGuess] = useState<string[]>(() => {
     const randomWord = wordList[ Math.floor(Math.random() * wordList.length) ]
@@ -18,14 +20,30 @@ const WordContextProvider = ( {children}: WordContextProviderProps ) => {
 
   const [guessedLetter, setGuessedLetter] = useState<string[]>([])
 
-  useEffect(() => {
+  const [guessWrongTime, setGuessWrongTime] = useState<number>(0)
+
+  useUpdateEffect(() => {
     console.log(guessedLetter)
-  })
+  },[guessedLetter])
+
+  useUpdateEffect(() => {
+
+    console.log('You wrong ' + guessWrongTime + ' time.')
+
+    if(guessWrongTime === 6){
+      setGameStatus("lose")
+      console.log('gameover')
+    }
+  }, [guessWrongTime])
 
   const contextValue: WordContext = {
+    gameStatus, 
+    setGameStatus,
     wordToGuess,
     guessedLetter,
     setGuessedLetter,
+    guessWrongTime,
+    setGuessWrongTime,
   }
 
   return (
--- src/Context/useWord.ts
+++ src/Context/useWord.ts
@@ -1,9 +1,15 @@
 import { createContext, useContext } from "react";
 
+export type GameStatus = "playing" | "win" | "lose"
+
 export interface WordContext {
+  gameStatus: string, 
+  setGameStatus: React.Dispatch<React.SetStateAction<GameStatus>>,
   wordToGuess: string[],
   guessedLetter: string[],
   setGuessedLetter: React.Dispatch<React.SetStateAction<string[]>>
+  guessWrongTime: number
+  setGuessWrongTime: React.Dispatch<React.SetStateAction<number>>
 }
 
 export const wordContext = createContext({} as WordContext)
