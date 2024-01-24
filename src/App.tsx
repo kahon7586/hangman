033612214586ac9a316678854b631a413cabab97
Kahon7586 <kahon7586@gmail.com>
2024-01-24 13:02:44 +0800

    trivial change for title

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import HangmanWord from './Components/HangmanWord'
 import HangmanKeyboard from './Components/HangmanKeyboard'
 import WordContextProvider from './Context/WordContextProvider'
 import { useWord } from './Context/useWord'
+import { useEffect, useState } from 'react'
 
 function App() {
 
   return (
     <WordContextProvider>
       <div className='interface'>
-        <div className='result'>Lose Win</div>
+        Word Guess
         <HangmanDrawing/>
         <HangmanWord />
         <HangmanKeyboard />
